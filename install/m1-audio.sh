0f143166e576ddd3410d8fe20b5c3813c13e6ef7
charles-park <charles.park@hardkernel.com>
2022-12-13 08:26:14 +0000

    mixer control add

--- install/m1-audio.sh
+++ install/m1-audio.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# list of playback device
+# aplay -l
+
+# mixer control path
+# amixer -c 1
+
+# mixer control path set
+amixer -c 1 sset 'Playback Path' 'HP'
+
 while true
 do
 	aplay -Dhw:1,0 /root/m1-server/piano.wav
